a6f15745cc8c05eec66c7bb0465ad3caf1d92862
kt <kthorat158@gmail.com>
2023-04-21 20:45:38 +0530

    plans section completed

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./Plans.css"
+import { plansData } from "../../data/plansData"
+import whiteTick from "../../assets/whiteTick.png"
+const Plans = () => {
+  return (
+    <div className='plans-container'>
+      <div className='plans-header' style={{ gap: '2rem' }}>
+        <span className='stroke-text'>our</span>
+        <span> Monthly </span>
+        <span className='stroke-text'>Plans</span>
+      </div>
+      {/* plans card */}
+      <div className='plans'>
+        {plansData.map((plan, i) => (
+          <div className='plan' key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>₹{plan.price}</span>
+            {/* plans feature */}
+            <div className='features'>
+              {plan.features.map((features, i) => (
+                <div className='feature'>
+                  <img src={ whiteTick } alt="whiteTick"></img>
+                  <span key={i}>{features}</span>
+                </div>
+              ))}
+            </div>
+            <div>
+            <button className='btn'>Join Now</button>
+            </div>
+          </div>
+
+        ))}
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Plans
--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,6 +4,7 @@ import image1 from "../../assets/image1.png"
 import image2 from "../../assets/image2.png"
 import image3 from "../../assets/image3.png"
 import image4 from "../../assets/image4.png"
+import tick from "../../assets/tick.png"
 
 
 const Reasons = () => {
@@ -14,6 +15,32 @@ const Reasons = () => {
     <img src={image2} alt="mens images"></img>
     <img src={image3} alt="mens images"></img>
     <img src={image4} alt="mens images"></img>
+    </div>
+    <div className='right-r'>
+      
+      <span>Some Reasons</span>
+      <div>
+        <span className='stroke-text'>why </span>
+        <span>Choose Us</span>
+      </div>
+      <div className='details-r'>
+        <div>
+          <img src={tick} alt="tick"></img>
+          <span>14+ Expert Coachs</span>
+        </div>
+        <div>
+          <img src={tick} alt="tick"></img>
+          <span>10+ programs</span>
+        </div>
+        <div>
+          <img src={tick} alt="tick"></img>
+          <span>1 free program for new members</span>
+        </div>
+        <div>
+          <img src={tick} alt="tick"></img>
+          <span>train faster and smarter than before</span>
+        </div>
+      </div>
     </div>
 
     </div>
